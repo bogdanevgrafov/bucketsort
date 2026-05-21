f30ca5e8b35089030e8a1680eb7427d494174ff7
Bogdan Evgrafov <bogdanevgrafov@bk.ru>
2026-05-21 20:35:00 +0300

    Update bucketsort.py

--- bucketsort.py
+++ bucketsort.py
@@ -1,50 +1,111 @@
+import time
+from random import randint
+
+import pandas as pd
+
 def insertion_sort(array):
     n = len(array)
 
+    operations = 0
     for i in range(1, n):
         for j in range(i, 0, -1):
             if array[j - 1] > array[j]:
                 array[j], array[j - 1] = array[j - 1], array[j]
+            operations += 1
 
-    return array
+    return array, operations
 
 
 def bucket_sort(array):
+    start = time.time()
+
+    operations = 0
     len_array = len(array)
 
     if len_array == 0:
         return array
 
-    # Создание бакетов
     buckets = [[] for i in range(len_array)]
 
     min_val = array[0]
     for element in array:
+        operations += 1
+
         if element < min_val:
             min_val = element
 
     max_val = array[0]
     for element in array:
+        operations += 1
         if element > max_val:
             max_val = element
 
-    # Проверка списка на отсортированность
+    # Проверка на отсортированность, чтобы не сортировать список зря
     if min_val == max_val:
-        return array
+        end = time.time()
+
+        return array, operations, end-start
 
     for num in array:
+        operations += 1
+
         normalize = (num - min_val) / (max_val - min_val + 0.1)
         bucket_number = int(len_array * normalize)
 
         buckets[bucket_number].append(num)
 
     for i in range(len_array):
-        buckets[i] = insertion_sort(buckets[i])
+        sort = insertion_sort(buckets[i])
+        buckets[i] = sort[0]
+        operations += sort[1]
 
     output = []
     for bucket in buckets:
         output.extend(bucket)
 
-    return output
+    end = time.time()
 
-print(bucket_sort([7, 3, 5, 2, 1]))
+    return output, operations, end-start
+
+
+def write_to_excel(arrs):
+    df = pd.DataFrame(columns=('Длина', "Количество операций", "Время"))
+    for arr in arrs:
+        func = bucket_sort(arr)
+        sorted_array = func[0]
+        operations = func[1]
+        times = f'{func[2]:.7f}'
+        df.loc[len(df)] = [len(arr), operations, times]
+
+    df["Время"] = df["Время"].astype(float)
+    df.to_excel('sorted_data.xlsx', index=False)
+
+def create_data():
+    with open('not_sorted_data.txt', 'w') as f:
+        for i in range(100, 10001, 100):
+            f.write(str([randint(-10000, 10000) for _ in range(i)]) + '\n')
+
+def sort_data(i, j):
+    arrs = [list(map(int, x[1:-2].split(', '))) for x in open('not_sorted_data.txt', 'r')]
+
+    for arr in arrs:
+        print(bucket_sort(arr)[i:j])
+
+choice = input(
+      '1) Создать файл с данными\n'
+      '2) Отсортировать данные из файла (если они уже имеются)\n'
+      'Выберите действие:\n'
+)
+if choice == '1':
+    create_data()
+elif choice == '2':
+    choice2 = input(
+        '\n1) Вывести отсортированные списки\n'
+        '2) Вывести только показатели (количество операций, время)\n'
+        'Выберите действие:\n'
+    )
+    if choice2 == '1':
+        sort_data(0, 1)
+    elif choice2 == '2':
+        sort_data(1, 3)
+        
