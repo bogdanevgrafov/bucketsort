8399aa1c8f4b692521d5cd288c9562956410930f
Bogdan Evgrafov <bogdanevgrafov@bk.ru>
2026-05-21 20:42:55 +0300

    Update bucketsort.py

--- bucketsort.py
+++ bucketsort.py
@@ -72,18 +72,19 @@ def write_to_excel(arrs):
     df = pd.DataFrame(columns=('Длина', "Количество операций", "Время"))
     for arr in arrs:
         func = bucket_sort(arr)
-        sorted_array = func[0]
         operations = func[1]
         times = f'{func[2]:.7f}'
         df.loc[len(df)] = [len(arr), operations, times]
 
     df["Время"] = df["Время"].astype(float)
-    df.to_excel('sorted_data.xlsx', index=False)
+    df.to_excel('sorted_databook.xlsx', index=False)
+
 
 def create_data():
     with open('not_sorted_data.txt', 'w') as f:
         for i in range(100, 10001, 100):
             f.write(str([randint(-10000, 10000) for _ in range(i)]) + '\n')
+
 
 def sort_data(i, j):
     arrs = [list(map(int, x[1:-2].split(', '))) for x in open('not_sorted_data.txt', 'r')]
@@ -91,21 +92,33 @@ def sort_data(i, j):
     for arr in arrs:
         print(bucket_sort(arr)[i:j])
 
-choice = input(
-      '1) Создать файл с данными\n'
-      '2) Отсортировать данные из файла (если они уже имеются)\n'
-      'Выберите действие:\n'
-)
-if choice == '1':
-    create_data()
-elif choice == '2':
-    choice2 = input(
-        '\n1) Вывести отсортированные списки\n'
-        '2) Вывести только показатели (количество операций, время)\n'
-        'Выберите действие:\n'
+    return arrs
+
+def menu():
+    choice = input(
+          '1) Создать файл с данными\n'
+          '2) Отсортировать данные из файла (если они уже имеются)\n'
+          'Выберите действие:\n'
     )
-    if choice2 == '1':
-        sort_data(0, 1)
-    elif choice2 == '2':
-        sort_data(1, 3)
-        
+    if choice == '1':
+        create_data()
+    elif choice == '2':
+        choice2 = input(
+            '\n1) Вывести отсортированные списки\n'
+            '2) Вывести только показатели (количество операций, время)\n'
+            'Выберите действие:\n'
+        )
+        if choice2 == '1':
+            a = sort_data(0, 1)
+        elif choice2 == '2':
+            a = sort_data(1, 3)
+    choice3 = input(
+        'Хотите записать данные в excel?\n'
+        '1) Да\n'
+        '2) Нет\n'
+        'Выберите действие:'
+    )
+    if choice3 == '1':
+        write_to_excel(a)
+
+menu()
